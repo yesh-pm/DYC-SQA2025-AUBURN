47fd348c25cd8833303b3208acdb57f3385f872f
yesh-pm <yeshenia.pm@gmail.com>
2025-04-24 18:24:09 -0500

    Update fuzz.py

--- REPO.md
+++ REPO.md
@@ -1,7 +1,5 @@
 SQA Project Report
+
 1. Create a Git Hook that will run and report all security weaknesses in the project in a CSV file whenever a Python file is changed and committed. (Campbell)
-   - report goes here
-3. Create a fuzz.py file that will automatically fuzz 5 Python methods of your choice. Report any bugs you discovered by the fuzz.py file. fuzz.py will be automatically executed from GitHub actions. (Yeshenia)
-   - report goes here
-5. Integrate forensics by modifying 5 Python methods of your choice. (Dorothy)
-   - report goes here
+2. Create a fuzz.py file that will automatically fuzz 5 Python methods of your choice. Report any bugs you discovered by the fuzz.py file. fuzz.py will be automatically executed from GitHub actions. (Yeshenia)
+3. Integrate forensics by modifying 5 Python methods of your choice. (Dorothy)
--- fuzz.py
+++ fuzz.py
@@ -1,31 +1,59 @@
-#4-function calculator
-def simpleCalc(n1, n2, operation):
-    res = 0
-    if (operation == '+'):
-        res = n1 + n2
-    elif operation == '-':
-        res = n1 - n2
-    elif operation == '*':
-        res = n1 * n2
-    elif operation == '/':
-        res = n1 / n2
-    return res
+import random
+import re
+import math
+import string
 
-def checkNonOperation():
-    operation_ = '='
-    simpleCalc(50, 5, operation_)
+#Even or Odd
+def fuzzEven():
+    try:
+        n = random.randint(-100, 100)
+        print(f"Is {n} even?")
+        if (n % 2) == 0:
+            print("Yes")
+    except Exception as e:
+        print("Error in fuzzEven", e)
 
-def calcFuzzer():
-    checkNonOperation()
+def fuzzOdd():
+    try:
+        n = random.randint(-100, 100)
+        print(f"Is {n} odd?")
+        if n % 2 == 1:
+            print("Yes")
+    except Exception as e:
+        print("Error in fuzzOdd", e)
+
+def fuzzChance():
+    try:
+        n1 = random.randint(0, 100)
+        n2 = random.randint(0, 100)
+        if n1 == n2:
+            print("Match")
+    except Exception as e:
+        print("Error in fuzzChance", e)
+
+def fuzzSqrt():
+    try:
+        num = random.randint(-100, 100)
+        math.sqrt(num)
+    except ValueError:
+        pass
+    except Exception as e:
+        print("Error in fuzzSqrt", e)
+
+def fuzzMatch():
+    try:
+        pattern = ''.join(random.choices(string.printable, k=3))
+        rstring = ''.join(random.choices(string.printable, k=8))
+        re.match(pattern, rstring)
+    except re.error:
+        pass
+    except Exception as e:
+        print("Error in fuzzMatch", e)
 
 if __name__ == '__main__':
-    n1, n2, op = 50, 5, '+'
-    data = simpleCalc(n1, n2, op)
-    print('Operation: {}\nResult: {}'.format(op, data))
-    calcFuzzer()
-
-def greaterThan(var, comp):
-    if (var > comp):
-        return True
-    elif var < comp:
-        return False
+    for i in range(10):
+        fuzzEven()
+        fuzzOdd()
+        fuzzChance()
+        fuzzSqrt()
+        fuzzMatch()
